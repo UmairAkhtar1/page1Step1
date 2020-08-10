06b3e1b2da0e757442d9133fe141b8bf6617b75d
UmairAkhtar1 <56793554+UmairAkhtar1@users.noreply.github.com>
2020-08-11 04:02:23 +0500

    publish my site
    i'm happy

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page1step1</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <style type="text/css">
+  	body {
+	  font-family: '[Gilroy]',sans-serif !important;
+	}
+  </style>
+</head>
+
+<body>
+
+	<div class="container-fluid" style="background-color:black;">
+		<nav class="navbar navbar-expand-lg">
+			<i class="fa fa-bars a1" aria-hidden="true" style="color:yellow;background-color:#343430;padding:10px;border-radius:20px;"></i>
+
+			<p class="a2" style="color:white;margin-top:20px;">Lorem ipsum dolor sit amet, consectetur adipisic...</p>
+			<p class="a3" style="color:white;margin-top:20px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+			<i class="fa fa-envelope a4" aria-hidden="true" style="font-size:35px;color:#787878;"></i>
+			<i class="fa fa-user a5" aria-hidden="true" style="font-size:35px;color:#787878;"></i>
+			<div class="a6">
+				<p class="text-center" style="margin-top:8px;color:white;"><strong>Welcome</strong></p>
+				<p class="text-center" style="margin-top:-1rem;color:#d0d0d0">Peter Parker</p>
+			</div>
+			
+		</nav>
+	</div>
+	<img src="shape3.png" width="20" style="margin-left:2.3rem;margin-top:-1.5rem">
+	<div class="container-fluid" style="background-color:#fff001;margin-top:-1.6rem;">
+		<nav class="navbar navbar-expand-lg">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <i class="fa fa-bars" aria-hidden="true"></i>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <center>
+		    	<ul class="navbar-nav mr-auto" style="margin-bottom:-0.5rem;">
+				<li class="nav-item active n1">
+					<p class="text-center mt-2 "><img src="user.png" class="img-responsive" style="background-color:#fff997;color:black;width:48px;border-radius:17px;padding:11px;"></i></p>
+					<a class="nav-link" href="#" style="font-size:20px;margin-top:-20px;color:black;"><strong>Manage<br>Account</strong><span class="sr-only">(current)</span></a>
+				</li>
+		      	<li class="nav-item active n2" style=";border-bottom:5px solid black;">
+					<p class="text-center mt-2 "><img src="web-development.png" class="img-responsive" style="background-color:#fff997;color:black;width:48px;border-radius:17px;padding:11px;"></i></p>
+					<a class="nav-link" href="#" style="font-size:20px;margin-top:-20px;color:black;"><strong>Manage<br>Data</strong></a>
+				</li>
+				<li class="nav-item active n3">
+					<p class="text-center mt-2 "><img src="template.png" class="img-responsive" style="background-color:#fff997;color:black;width:48px;border-radius:17px;padding:11px;"></i></p>
+					<a class="nav-link" href="#" style="font-size:20px;margin-top:-20px;color:black;"><strong>Manage<br>Template</strong></a>
+				</li>
+				<li class="nav-item active n4">
+					<p class="text-center mt-2"><img src="test.png" class="img-responsive" style="background-color:#fff997;color:black;width:48px;border-radius:17px;padding:11px;"></i></p>
+					<a class="nav-link" href="#" style="font-size:20px;margin-top:-20px;color:black;"><strong>Manage<br>Results</strong></a>
+				</li>
+				<li class="nav-item active n5">
+					<p class="text-center mt-2 "><img src="bell.png" class="img-responsive" style="background-color:#fff997;color:black;width:48px;border-radius:17px;padding:11px;"></i></p>
+					<a class="nav-link" href="#" style="font-size:20px;margin-top:-20px;color:black;"><strong>Manage<br>Alerts</strong></a>
+				</li>
+		    	</ul>
+		    </center>
+		  </div>
+		</nav>
+	</div>
+
+	<div class="container">
+		<p class="text-center" style="margin-top:20px;font-size:30px;margin-bottom:30px;"><strong>Select A Data Type to Manage</strong></p>
+	</div>
+
+	<div class="container">
+		<center >
+			<div class="row">
+			<div class="col-md-2 offset-md-1 b1">
+				<div class="card b11" style="width:10rem;height:10.5rem;border-color:black;">
+				  <center style="background-color: #f9f9f9;"><img class="img-responsive" src="Happyuser10134.png" alt="Option1" style="width:50px;margin-top:30px;"></center>
+				  <div class="card-body">
+				    <p class="card-text text-center"><strong>Option 1</strong></p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-2 b2">
+				<div class="card b12" style="width:10rem;height: 10.5rem;border-color:black;">
+		  <center style="background-color: #f9f9f9;"><img class="card-img-top img-responsive" src="Happyuser10134.png" alt="Option1" style="width:50px;margin-top:30px;"></center>
+		  <div class="card-body">
+		    <p class="card-text text-center"><strong>Option 2</strong></p>
+		  </div>
+		</div>
+			</div>
+			<div class="col-md-2 b3">
+				<div class="card b13" style="width:10rem;height: 10.5rem;border-color:black;">
+		  <center style="background-color: #f9f9f9;"><img class="card-img-top img-responsive" src="paper (1).png" alt="Option1" style="width:50px;margin-top:30px;"></center>
+		  <div class="card-body">
+		    <p class="card-text text-center"><strong>Option 3</strong></p>
+		  </div>
+		</div>
+			</div>
+			<div class="col-md-2 b4">
+				<div class="card b14" style="width:10rem;height: 10.5rem;border-color:black;">
+		  <center style="background-color: #f9f9f9;"><img class="card-img-top img-responsive" src="text.png" alt="Option1" style="width:50px;margin-top:30px;"></center>
+		  <div class="card-body">
+		    <p class="card-text text-center"><strong>Option 4</strong></p>
+		  </div>
+		</div>
+			</div>
+			<div class="col-md-2 b5">
+				<div class="card b15" style="width:10rem;border-color:none;background-color:#fff001;">
+		  <center ><img src="plus.png" style="width:49px;padding:11px;border-radius:25px;margin-top:30px;background-color:#fff997"></center>
+		  <div class="card-body">
+		    <p class="card-text text-center"><strong>Click to Create<br>New Template</strong></p>
+		  </div> 
+		</div>
+			</div>
+		</div>
+		</center>
+	</div>
+
+	<div class="container-fluid" style="margin-top:60px;background-color:#f9f9f9;">
+		<hr>
+		<div class="row justify-content-between">
+	    <div class="col-md-6">
+	      <p>Terms & Condition</p>
+	    </div>
+	    <div class="col-md-6">
+	      <p class="float-right">All Services | Policy</p>
+	    </div>
+	  </div>
+	</div>
+
+</body>
+</html>
